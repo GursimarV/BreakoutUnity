c1fcad476139b0e6e4600fd64b96a378d8ea3572
Gursimar Virdi <385491@colum.edu>
2024-04-02 15:06:38 -0500

    BreakoutUnity

---
+++ BreakoutUnityGV/Assets/Scripts/BallController.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 Direction;
+    public float Speed;
+
+    public GameObject Paddle;
+    public GameObject LostButton;
+    private bool PlayerRespawn = true;
+    private bool CanRespawn = true;
+    private Vector3 BallRespawn;
+
+    private Rigidbody2D rb2D;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        Util.GetComponentIfNull<SpriteRenderer>(this, ref spriteRenderer); 
+        rb2D = GetComponent<Rigidbody2D>();//Check for null?
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        BallRespawn = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RespawnOrLose();
+    }
+
+    private void RespawnOrLose()
+    {
+        if (this.gameObject.transform.position.y < (Paddle.transform.position.y - .8) && PlayerRespawn == true)
+        {
+            ScoreManager.Lives -= 1;
+            PlayerRespawn = false;
+            if (CanRespawn == true)
+            {
+                this.gameObject.transform.position = new Vector3(Paddle.transform.position.x, BallRespawn.y, 0);
+                CanRespawn = false;
+                PlayerRespawn = true;
+            }
+
+            if (ScoreManager.Lives <= 0)
+            {
+                LostButton.SetActive(true);
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// FixedUpdate uses physics
+    /// </summary>
+    void FixedUpdate()
+    {
+        if (rb2D != null)
+        {
+            //Keep on screen
+            this.rb2D.position = Util.BounceOffWalls(this.transform.position,
+                spriteRenderer.bounds.size.x - 1,
+                spriteRenderer.bounds.size.y - 1, ref this.Direction);
+        }
+
+        rb2D.MovePosition(rb2D.position + Direction * Speed * Time.fixedDeltaTime);
+    }
+
+    /// <summary>
+    /// Adds a bit of randomness to the ball bounce collision
+    /// </summary>
+    public void UpdateBallCollisionRandomFuness()
+    {
+        /// 
+        /// Adds a bit of entropy to bounce nothing should be perfect
+        /// 
+        /// 
+        this.Direction.y *= GetReflectEntropy();
+    }
+
+
+    private float GetReflectEntropy()
+    {
+        return -1 + ((Random.Range(0, 3) - 1) * 0.1f); //return -.9, -1 or -1.1
+    }
+
+    public void RelfectY()
+    {
+        this.Direction.y *= -1;
+    }
+
+    public void RelfectX()
+    {
+        this.Direction.x *= -1;
+    }
+
+
+}
---
+++ BreakoutUnityGV/Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("I'm done right now, but I will be back!!!");
+        Application.Quit();
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
